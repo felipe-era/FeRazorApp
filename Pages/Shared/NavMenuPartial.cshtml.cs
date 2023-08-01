3aa70a5a8b7fab13cff1298f4fdc5cbd8744d876
feveloper <felip@FEVOLEPER>
2023-08-01 20:03:30 -0300

    Tests

---
+++ Pages/Shared/NavMenuPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FeRazorApp.Pages.Shared
+{
+    public class NavMenuPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
