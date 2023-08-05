0abbaf2a4e179dcea8eadab1315b49abc41fb848
feveloper <felip@FEVOLEPER>
2023-08-04 22:58:57 -0300

    Mapping Test

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -6,13 +6,21 @@ public class CategoriesModel : PageModel
 
     public List<Category> CategoryList { get; set; } = new();
 
-    public void OnGet()
+    public void OnGet(int skip, int take)
     {
+        var lstAuxCategory = new List<Category>();
         //await Task.Delay(5000);
         for (int i = 0; i < 100; i++)
         {
             CategoryList.Add(new Category(i, Title: "Categoria", Price: i * 9.9M));//M = Decimal  
+            lstAuxCategory.Add(new Category(i, Title: "Categoria", Price: i * 9.9M));//M = Decimal  
+
         }
+        CategoryList = lstAuxCategory
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+
     }
     public void OnPost()
     {
