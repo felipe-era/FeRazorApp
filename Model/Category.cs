e2c219f3489d0dc9b587c778f3567e6faf5b6d52
feveloper <felip@FEVOLEPER>
2023-08-08 20:22:05 -0300

    add Migrations, DbContext, EF, SqlServer up Sucess

---
+++ Model/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeRazorApp.Model;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
