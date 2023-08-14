5ed87ed7fe36d26f16589d14020856ed3b85f563
feveloper <felip@FEVOLEPER>
2023-08-14 19:08:07 -0300

    bootstrap end, validation client side

--- Model/Category.cs
+++ Model/Category.cs
@@ -6,7 +6,10 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+    [Display(Name = "Nome")]
     [Required]
     public string Name { get; set; }
+    [Display(Name="Ordem Exibição")]
+    [Range(1,100, ErrorMessage ="Valor deve estar entre 1 a 100!")]
     public int DisplayOrder { get; set; }
 }
