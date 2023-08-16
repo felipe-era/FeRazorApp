cc685da42d8e2ab337c32ab747d08f1e3a9cd965
felipec <felipeedc29@gmail.com>
2023-08-16 15:46:17 -0300

    Edit, icons, todo..

---
+++ Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using FeRazorApp.Data;
+using FeRazorApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FeRazorApp.Pages.Categories;
+
+[BindProperties]
+public class EditModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Category Category { get; set; }
+
+    public EditModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet(int id)
+    {
+        Category = _db.Category.Find(id);
+        //Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+        //Category = _db.Category.SingleOrDefault(u=>u.Id==id);
+        //Category = _db.Category.Where(u=>u.Id==id).FirstOrDefault();
+
+    }
+
+    public async Task<IActionResult> OnPost(Category category)
+    {
+        if (Category.Name == Category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError(Category.Name, "DisplayOrder e Nomes não podem ser iguais");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Category.Update(category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+        return Page();
+    }
+}
