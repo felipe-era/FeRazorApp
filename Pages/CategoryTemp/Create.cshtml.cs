472f8cace45503aef6fb5c1ed822c7c08dfee6bf
feveloper <felip@FEVOLEPER>
2023-08-19 20:16:50 -0300

    Scaffold on CategoryTemp see

---
+++ Pages/CategoryTemp/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FeRazorApp.Data;
+using FeRazorApp.Model;
+
+namespace FeRazorApp.Pages.CategoryTemp
+{
+    public class CreateModel : PageModel
+    {
+        private readonly FeRazorApp.Data.ApplicationDbContext _context;
+
+        public CreateModel(FeRazorApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Category == null || Category == null)
+            {
+                return Page();
+            }
+
+            _context.Category.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/CategoryTemp/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FeRazorApp.Data;
+using FeRazorApp.Model;
+
+namespace FeRazorApp.Pages.CategoryTemp
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FeRazorApp.Data.ApplicationDbContext _context;
+
+        public DetailsModel(FeRazorApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      public Category Category { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Category = category;
+            }
+            return Page();
+        }
+    }
+}
