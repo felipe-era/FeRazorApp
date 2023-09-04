08388ac48106a2cae137f93a003c4c69187a87a9
feveloper <felip@FEVOLEPER>
2023-09-04 16:37:21 -0300

    IdentityUser, Auth,  async OnPost

--- Pages/Login/Register.cshtml.cs
+++ Pages/Login/Register.cshtml.cs
@@ -1,4 +1,5 @@
 using FeRazorApp.Model;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,8 +7,47 @@ namespace FeRazorApp.Pages.Login;
 
 public class RegisterModel : PageModel
 {
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly SignInManager<IdentityUser> signInManager;
+
+    [BindProperty]
     public Register Model { get; set; }
+
+    public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        this.signInManager = signInManager;
+        this.userManager = userManager;
+    }
     public void OnGet()
     {
+    }
+
+    public async Task<IActionResult>OnPostAsync()
+    { 
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser()
+            {
+                UserName = Model.Email,
+                Email = Model.Email
+            };
+
+            var result = userManager.CreateAsync(user, Model.Password);
+            if (result.IsCompletedSuccessfully)//verificar outras opções do result. (succeded?)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToPage("Index");
+            }
+
+            foreach (var error in result.Result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+
+            }
+
+        }
+
+        return Page();
+
     }
 }
